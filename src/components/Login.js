390cd592fd9d36e8b861f6a24f3018e0d3de291b
azatprojazz <azatprojazz@gmail.com>
2023-03-31 18:40:07 +0300

    fix: добавить jsx и стейты в компонент Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,52 @@
-function Login() {
+import { useState, useEffect } from 'react';
+
+function Login({ onSignin }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    setEmail('');
+    setPassword('');
+  }, []);
+
+  function handleChangeEmail(evt) {
+    setEmail(evt.target.value);
+  }
+
+  function handleChangePassword(evt) {
+    setPassword(evt.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSignin(email, password);
+  }
+
   return (
-    <div>
-      <h2>Вход</h2>
-    </div>
+    <section className="section">
+      <h2 className="section__header">Вход</h2>
+      <form className="section__form" onSubmit={handleSubmit}>
+        <input
+          className="section__input"
+          autoComplete="email"
+          type="email"
+          value={email}
+          onChange={handleChangeEmail}
+          placeholder="Email"
+        />
+        <input
+          className="section__input"
+          autoComplete="current-password"
+          type="password"
+          value={password}
+          onChange={handleChangePassword}
+          placeholder="Пароль"
+        />
+        <button className="section__btn opacity-on-hover" type="submit">
+          Войти
+        </button>
+      </form>
+    </section>
   );
 }
 
