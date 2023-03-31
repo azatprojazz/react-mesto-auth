b34841896389ce0d71034e655ce8d83db69c4832
azatprojazz <azatprojazz@gmail.com>
2023-03-31 18:35:53 +0300

    fix: добавить jsx и стейты к компоненту Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,59 @@
-function Register() {
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+function Register({ onSignup }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    setEmail('');
+    setPassword('');
+  }, []);
+
+  function handleChangeEmail(evt) {
+    setEmail(evt.target.value);
+  }
+
+  function handleChangePassword(evt) {
+    setPassword(evt.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSignup(email, password);
+  }
+
   return (
-    <div>
-      <h2>Регистрация</h2>
-    </div>
+    <section className="section">
+      <h2 className="section__header">Регистрация</h2>
+      <form className="section__form" onSubmit={handleSubmit}>
+        <input
+          className="section__input"
+          autoComplete="email"
+          type="email"
+          value={email}
+          onChange={handleChangeEmail}
+          placeholder="Email"
+        />
+        <input
+          className="section__input"
+          autoComplete="new-password"
+          type="password"
+          value={password}
+          onChange={handleChangePassword}
+          placeholder="Пароль"
+        />
+        <button className="section__btn opacity-on-hover" type="submit">
+          Зарегистрироваться
+        </button>
+      </form>
+      <p className="section__question">
+        Уже зарегистрированы?{' '}
+        <Link to="/sign-in" className="section__link opacity-on-hover">
+          Войти
+        </Link>
+      </p>
+    </section>
   );
 }
 
